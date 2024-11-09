74d36e877787fb0218f4bd9b1b3866a9ec104f46
v-andrieiev <61801638+v-andrieiev@users.noreply.github.com>
2024-11-10 01:57:03 +0200

    SearchService. Sync communication

--- src/AuctionService/Controllers/AuctionController.cs
+++ src/AuctionService/Controllers/AuctionController.cs
@@ -2,6 +2,7 @@ using AuctionService.Data;
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,13 +13,16 @@ namespace AuctionService.Controllers;
 public class AuctionController(AuctionDbContext context, IMapper mapper): ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<AuctionDto>>> GetALlAuctions()
+    public async Task<ActionResult<List<AuctionDto>>> GetALlAuctions(string date)
     {
-        var auctions = await context.Auctions
-            .Include(x => x.Item)
-            .OrderBy(x => x.Item.Make)
-            .ToListAsync();
-        return mapper.Map<List<AuctionDto>>(auctions);
+        var query = context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
+
+        if (!string.IsNullOrEmpty(date))
+        {
+            query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+        }
+
+        return await query.ProjectTo<AuctionDto>(mapper.ConfigurationProvider).ToListAsync();
     }
     
     [HttpGet("{id}")]
---
+++ src/SearchService/Services/AuctionServiceHttpClient.cs
@@ -0,0 +1,18 @@
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services;
+
+public class AuctionServiceHttpClient(HttpClient httpClient, IConfiguration config)
+{
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>()
+            .Sort(x => x.Descending(x => x.UpdatedAt))
+            .Project(x => x.UpdatedAt.ToString())
+            .ExecuteFirstAsync();
+        
+        return await httpClient.GetFromJsonAsync<List<Item>>(config["AuctionServiceUrl"] 
+            + "/api/auctions?data=" + lastUpdated);
+    }
+}
